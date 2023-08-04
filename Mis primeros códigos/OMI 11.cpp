e184143f02f9837c768e0b4de27c9cca7e77db81
Rambloxzzx <130252084+Rambloxzzx@users.noreply.github.com>
2023-08-04 17:55:51 -0600

    Add files via upload

---
+++ Mis primeros códigos/OMI 11.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n=0;
+    cin >> n;
+    while(n>0){
+        cout << n << ' ';
+        n--;
+    }
+    return 0;
+}
